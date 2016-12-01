dcd41d6a4ef46b74906bdf7e172ae07b56cde7ba
awwcorey <imgaliusha@gmail.com>
2016-12-01 15:11:11 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,18 @@
+a = input()
+b = input()
+c = input()
+p = int(a)
+x = int(b, p)
+k = int(c)
+n = ''
+
+while x > 0:
+    y = str(x % k)
+    for k in range (9, 44):
+        if y == str(k):
+            y = chr(55+int(y))
+
+    n = y + n
+    x = int(x / k)
+
+print(n)
