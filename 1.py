97bcc7f9e8aa8613c5e4339370f04777e46d27b3
awwcorey <imgaliusha@gmail.com>
2016-12-01 16:40:43 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -8,11 +8,11 @@ n = ''
 
 while x > 0:
     y = str(x % k)
-    for k in range (10, 37):
-        if y == str(k):
-            y = chr(55+int(y))
 
+    if int(y)>9:
+        y = chr(55 + int(y))
     n = y + n
     x = int(x / k)
+
 
 print(n)
